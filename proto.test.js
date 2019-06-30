69c0adfd8fbe4ddfe524bb53a1303a579912a0de
Andrew Fong <afong@slack-corp.com>
2019-06-29 23:45:32 -0700

    Add simpler prototype example

--- proto.test.js
+++ proto.test.js
@@ -1,4 +1,24 @@
 describe("Spying on protoypes", () => {
+  const A = { test: () => 1 };
+
+  const B = Object.create(A);
+
+  afterEach(() => {
+    jest.restoreAllMocks();
+  });
+
+  it("works with B", () => {
+    jest.spyOn(B, "test").mockReturnValue(3);
+    expect(B.test()).toEqual(3);
+  });
+
+  it("works with A", () => {
+    jest.spyOn(A, "test").mockReturnValue(2);
+    expect(B.test()).toEqual(2);
+  });
+});
+
+describe("Spying on class protoypes", () => {
   class A {
     test() {
       return 1;
